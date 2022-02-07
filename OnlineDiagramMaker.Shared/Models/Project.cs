0b8f9356b98e32e373348bf0c3ab2ca0254d09be
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2022-02-07 17:26:29 +0100

    ✨initialize project and models

---
+++ OnlineDiagramMaker.Shared/Models/Project.cs
@@ -0,0 +1,17 @@
+﻿namespace OnlineDiagramMaker.Shared.Models
+{
+#nullable disable
+    public class Project
+    {
+
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public string Name { get; set; }
+        public bool? IsActive { get; set; }
+        public DateTime dateTime { get; set; }
+        public string DataNodes { get; set; }
+        public string DataNodesConnectors { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
---
+++ OnlineDiagramMaker.Shared/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace OnlineDiagramMaker.Shared.Models
+{
+#nullable disable
+    public class User
+    {
+        public User()
+        {
+            Projects = new HashSet<Project>();
+        }
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Project> Projects { get; set;}
+    }
+}
