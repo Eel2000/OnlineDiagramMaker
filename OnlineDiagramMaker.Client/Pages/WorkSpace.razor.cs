a3f4d3fc5289a625d0d25f9f87e02bbd5760caec
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2022-02-15 11:52:43 +0100

    🚧Minor changes

--- OnlineDiagramMaker.Client/Pages/WorkSpace.razor.cs
+++ OnlineDiagramMaker.Client/Pages/WorkSpace.razor.cs
@@ -11,20 +11,45 @@ namespace OnlineDiagramMaker.Client.Pages
         SfDiagramComponent? diagram;
 
         DiagramObjectCollection<Node> nodes = default!;
+        DiagramObjectCollection<Connector> connectors = default!;
         ObservableCollection<string> shapes = default!;
+        ObservableCollection<string> connectorsShape = default!;
+
+        private bool collapseNavMenu = true;
+        private bool expandNavMenu = false;
+        private bool collapseConnnectors = false;
+        private bool expandConnnectorsShape = false;
+
+        private string? NavMenuCollapseCss => collapseNavMenu ? "collapse" : null;
+        private string? NavConnectorsCollapseCss => collapseConnnectors ? "collapse" : null;
 
         protected override void OnInitialized()
         {
             nodes = new DiagramObjectCollection<Node>();
-            //initilize palettes
+            connectors = new DiagramObjectCollection<Connector>();
+            //initializing palettes
             InitializePalettes();
             base.OnInitialized();
+        }
+
+        private void ExpandNavMenu()
+        {
+            collapseNavMenu = !collapseNavMenu;
+            expandNavMenu = !expandNavMenu;
+        }
+
+        private void ExpandOrCollapseConnectorsArea()
+        {
+            collapseConnnectors = !collapseConnnectors;
+            expandConnnectorsShape = !expandConnnectorsShape;
         }
 
         private void InitializePalettes()
         {
             var t = EnumHelper.GetEnumItems<BasicShapeType>();
+            var c = EnumHelper.GetEnumItems<ConnectorSegmentType>();
             shapes = new ObservableCollection<string>(t.Select(x => x.ToString()));
+            connectorsShape = new ObservableCollection<string>(c.Select(x => x.ToString()));
         }
 
         private void CreateBasicForm(string basic)
@@ -49,6 +74,21 @@ namespace OnlineDiagramMaker.Client.Pages
             nodes.Add(node);
         }
 
+        private void CreateConnector(string name)
+        {
+            var conShape = EnumHelper.GetValueOf<ConnectorSegmentType>(name);
+            Console.WriteLine(conShape.ToString(), "connector selected");
+            Connector connector = new Connector
+            {
+                ID = Guid.NewGuid().ToString(),
+                SourcePoint = new DiagramPoint() { X = 100, Y = 100 },
+                TargetPoint = new DiagramPoint() { X = 200, Y = 200 },
+                Constraints = ConnectorConstraints.Default | ConnectorConstraints.DragSegmentThumb,
+                
+            };
+            connectors.Add(connector);
+        }
+
         private void UndoDraw() => diagram?.Undo();
         private void RedoDraw() => diagram?.Redo();
 
@@ -60,7 +100,7 @@ namespace OnlineDiagramMaker.Client.Pages
             export.Region = DiagramPrintExportRegion.ClipBounds;
             export.PageWidth = 816;
             export.PageHeight = 1054;
-            export.Margin = new Margin() { Left=10, Right=10, Bottom=10, Top = 10 };
+            export.Margin = new Margin() { Left = 10, Right = 10, Bottom = 10, Top = 10 };
 
             await diagram.ExportAsync("diagram", DiagramExportFormat.PNG, export);
         }
@@ -68,13 +108,12 @@ namespace OnlineDiagramMaker.Client.Pages
         private async void PrindOrExportPdf()
         {
             double left = 10;
-            double top = 10;
+            double top = 30;
             double right = 10;
             double bottom = 10;
             double width = 480;
             double height = 500;
             bool multiplePage = true;
-            bool showPageBreak = true;
             DiagramPrintExportRegion region = DiagramPrintExportRegion.ClipBounds;
             PageOrientation orientation = PageOrientation.Portrait;
 
